00ecc6f434692edc507048c393b2cf170bf5ce5f
github username <github email address>
2019-05-17 23:01:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.getElementById('toy-collection');
 let addToy = false
 
 // YOUR CODE HERE
@@ -14,6 +15,90 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
+
+document.addEventListener("DOMContentLoaded",function(){
+  console.log('loaded');
+  displayToys();
+  toyForm.addEventListener('submit',createToy)
+})
+
+function displayToys(){
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(data => 
+    data.forEach(toy => {
+      let div = document.createElement('div');
+      div.className = 'card';
+      let h2 = document.createElement('h2');
+      h2.innerText = toy.name;
+      div.appendChild(h2);
+      let img = document.createElement('img');
+      img.className = 'toy-avatar';
+      img.src = toy.image;
+      div.appendChild(img);
+      let p = document.createElement('p');
+      p.innerText = toy.likes;
+      div.appendChild(p);
+      let button = document.createElement('button');
+      button.innerText = "Like <3";
+      button.className = "like-btn";
+      button.addEventListener('click',function(){
+        like(toy.id, toy.likes)
+      });
+      div.appendChild(button);
+      toyCollection.appendChild(div);
+
+
+    })   
+)}
+
+function like(id, likes){
+  let parsed = parseInt(likes);
+  //console.log(parsed);
+  let newNumberOfLikes = parsed + 1
+  //console.log(newNumberOfLikes);
+  fetch(`http://localhost:3000/toys/${id}`,{
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: newNumberOfLikes 
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {
+     let likes = document.getElementById('likes');
+     likes.innerText = data.likes;
+     //console.log(data);
+   
+  })
+}
+
+function createToy(e){
+  e.preventDefault();
+  console.log(e);
+  //debugger
+  fetch(`http://localhost:3000/toys/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+    body: JSON.stringify({
+      name: e.target[0].value,
+      image: e.target[1].value,
+      likes: 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+
+
+
+
 
 
 // OR HERE!
